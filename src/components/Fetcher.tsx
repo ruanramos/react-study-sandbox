e2bea5a092a3d0c829f41d3ed44fde7e222d36e7
Ruan Ramos <ruan.ramos@intelie.com.br>
2021-01-21 04:47:41 -0300

    Training some fetch

--- src/components/Fetcher.tsx
+++ src/components/Fetcher.tsx
@@ -3,43 +3,55 @@ import React, { useState } from "react";
 export const Fetcher = () => {
   const url = "https://randomuser.me/api/";
   const [users, setUsers] = useState([]);
+  const [pending, setPending] = useState(false);
 
   const fetchData = () => {
+    setPending(true);
+    console.log("STARTING FETCH")
     fetch(url)
       .then((res) => res.json())
       .then((u) => {
         setUsers([...users, u.results[0]]);
-        console.log(users);
+        setPending(false);
+        console.log("ENDING FETCH")
       });
   };
 
   const st = { display: "flex", color: "red" };
 
+  const userInfo = <div style={st}>
+    {users.map((u) => (
+      <div key={u.id.value}>
+        <div>
+          <img src={`${u.picture.large}`} />
+        </div>
+        <div>{`${u.name.title} ${u.name.first} ${u.name.last}`}</div>
+      </div>
+    ))}
+  </div>;
+
+  const userJson = <div>
+    {users.map((u) => (
+      <div key={u.id.value}>
+        <ul>
+          <li>
+            <div>{`${JSON.stringify(u)}`}</div>
+          </li>
+        </ul>
+      </div>
+    ))}
+  </div>;
+
+
   return (
     <div>
-      <button onClick={fetchData}>FETCH</button>
-      <div style={st}>
-        {users.map((u) => (
-          <div key={u.id.value}>
-            <div>
-              <img src={`${u.picture.large}`} />
-            </div>
-            <div>{`${u.name.title} ${u.name.first} ${u.name.last}`}</div>
-          </div>
-        ))}
-      </div>
-
-      <div>
-        {users.map((u) => (
-          <div key={u.id.value}>
-            <ul>
-              <li>
-                <div>{`${JSON.stringify(u)}`}</div>
-              </li>
-            </ul>
-          </div>
-        ))}
-      </div>
+      {pending ?
+        "LOADING...." :
+        <><button onClick={fetchData} onMouseUp={() => console.log(users)} >FETCH</button>
+          <div className="users">
+            {userInfo}
+          </div></>
+      }
     </div>
   );
 };
