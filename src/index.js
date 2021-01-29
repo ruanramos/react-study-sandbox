27b1c9e299c645b302906c0864d245d6c66eb1a9
Ruan Ramos <ruan.ramos@intelie.com.br>
2021-01-29 00:51:28 -0300

    Playing with react-resizable

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 import { Fetcher } from "./components/Fetcher";
 import GridGenerator, { MyGrid } from './components/GridGenerator'
 import styled from "styled-components";
+import { Resizable, ResizableBox } from 'react-resizable';
+import "react-resizable/css/styles.css";
+
 
 const rootElement = document.getElementById("root");
 
@@ -11,21 +14,48 @@ const T = () => {
   return (
     <div>
 
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map( i => <button key={i}></button>)}
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => <button key={i}></button>)}
 
     </div>
   );
 }
 
+const StyledSpan = styled.span`
+  border: solid 3px red;
+`;
+
+const MaBox = () => {
+  return (
+    <ResizableBox
+      className="box"
+      width={200}
+      height={200}
+      minConstraints={[100, 100]}
+      maxConstraints={[300, 300]}
+      axis={"both"}
+      resizeHandles={['sw', 'se', 'nw', 'ne', 'w', 'e', 'n', 's']}
+      handleSize={[8, 8]}
+      onResize={() => console.log("Resizing")}
+      className={"box"}
+    >
+      <StyledSpan>AAAAAAAAAAAAAAA</StyledSpan>
+    </ResizableBox>
+  );
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <div className="ble">
-      {/*<MyGrid bla="blablaaa" className="centered"/>*/}
-    <T classname="ble"></T>
-    </div>
-  </React.StrictMode>,
+    <StyledSpan>{window.innerWidth}</StyledSpan>
+    <MaBox>
+      <StyledSpan>{window.innerWidth}</StyledSpan>
+    </MaBox>
+    <ResizableBox
+      width={300}
+      height={300}
+      className="box"
+      resizeHandles={["sw", "se", "nw", "ne", "w", "e", "n", "s"]}
+    ></ResizableBox>
+
+  </React.StrictMode >,
   rootElement
 );
-
-
